07bb9dc4058b3db24a51d143f92be9bee4c07456
chidiebereojingwa <ojingwaoji@gmail.com>
2022-01-19 22:36:35 +0100

    Added my components to app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { Loader, Footer, Services, Welcom, Transactions, Navbar } from "./components"
+import { Loader, Footer, Services, Welcome, Transactions, Navbar } from "./components"
 
 const App =()=> {
 
 
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline'>
-        Hello World
-      </h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome/>
+      </div>
+      
+      
     </div>
   )
 }
