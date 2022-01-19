9dd198221bdf6b10478a0754fe49995baeeca242
chidiebereojingwa <ojingwaoji@gmail.com>
2022-01-19 22:09:35 +0100

    converted App.js to Arrow func

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App =()=> {
+
 
   return (
     <div className="App">
